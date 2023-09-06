deb6bf66ee859f0f4194b23b2b65596b10e072c0
Opeyemi Agunbiade <91081341+Dapesy@users.noreply.github.com>
2023-09-06 12:57:24 +0100

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+
+face_cascades = cv.CascadeClassifier('data/opencv-4.x/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+cap = cv.VideoCapture(0)
+
+fourcc = cv.VideoWriter_fourcc(*'X264')  # Define the codec (X264 for compactibility)
+output_filename = 'output1.mp4'  # Name of the output video file
+frame_width = int(cap.get(3))  # Width of the frames
+frame_height = int(cap.get(4))  # Height of the frames
+frame_rate = cap.get(cv.CAP_PROP_FPS)  # Frames per second (adjust as needed)
+
+out = cv.VideoWriter(output_filename, fourcc, frame_rate, (frame_width, frame_height))
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == True:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        face = face_cascades.detectMultiScale(gray, 1.1, 4)
+
+        # drawing rectangle around the face 
+        for (x,y,w,h) in face:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
+
+        out.write(frame)
+
+        cv.imshow('video',frame)
+        key = cv.waitKey(30) & 0xff
+        if key ==27:
+            break
+    else:
+        break
+cap.release()
+out.release()
+cv.destroyAllWindows()
